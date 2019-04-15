ce6ebc1c00f4aab40763ebc741d7619832c69996
JukeFA <45007798+JukeFA@users.noreply.github.com>
2019-04-15 09:44:55 -0400

    added database information for quick changing

--- Insert Multple Items into Database.php
+++ Insert Multple Items into Database.php
@@ -1,6 +1,6 @@
 <?
-mysql_connect(".....", "....", "...");
-mysql_select_db("...");
+mysql_connect("sql303.unaux.com", "unaux_23766826", "Startrek20!4");
+mysql_select_db("unaux_23766826_Class");
 
 $order = "INSERT INTO Contactinfo
     (FirstName, LastName, Address, Phone)
