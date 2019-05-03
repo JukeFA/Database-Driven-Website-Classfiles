c2d02394697ead14c0bdc6d05b2acc04bc0d24f3
JukeFA <juke1229@gmail.com>
2019-05-02 22:34:32 -0400

    added final project

---
+++ Final Project Website/purchase.php
@@ -0,0 +1,14 @@
+<?php
+$email=$_POST['Email'];
+$firstname=$_POST['FirstName'];
+$lastname=$_POST['LastName'];
+$address=$_POST['Address'];
+$phone=$_POST['Phone'];
+
+mysql_connect("sql111.unaux.com", "unaux_23772555", "lc9rfmooyz9") or die(mysql_error());
+mysql_select_db("unaux_23772555_photography") or die(mysql_error());
+mysql_query("INSERT INTO 'photolist'
+VALUES
+('$email', '$firstname','$lastname','$address','$phone')");
+Print "Your order has been placed";
+?>
