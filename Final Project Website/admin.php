29f06e516d070f62ba6361f7286d007a1cb29d46
JukeFA <45007798+JukeFA@users.noreply.github.com>
2019-05-06 14:05:39 -0400

    Update admin.php

--- Final Project Website/admin.php
+++ Final Project Website/admin.php
@@ -1,10 +1,12 @@
 <html>
+
 <head>
-<title>Read Database</title>
+<title>Average
+</title>
 <style type="text/css">
 body {
-    margin-left: 100px;
-    margin-right: 100px;
+	margin-left: 100px;
+	margin-top: 100px;
 }
 </style>
 </head>
@@ -12,45 +14,63 @@ body {
 <body>
 
 <?php
-    $db_host = "sql111.unaux.com";
-    $db_user = "unaux_23772555";
-    $db_pwd = "lc9rfmooyz9";
+   $db_host = "sql111.unaux.com";
+   $db_user = "unaux_23772555";
+   $db_pwd = "lc9rfmooyz9";
 
-    $database = "unaux_23772555_photography";
-    $table = "photolist";
+   $database = "unaux_23772555_photography";
+   $table = "photolist";
 
-    if (!mysql_connect($db_host, $db_user, $db_pwd))
-        die("Can't access server");
+if (!mysql_connect($db_host, $db_user, $db_pwd))
+     die("Can't connect to database");
 
-    if (!mysql_select_db($database))
-        die("Can't select database");
+if (!mysql_select_db($database))
+     die("Can't select database");
 
-    // sending query
-    $result = msql_query("SELECT * FROM {$table}");
-    if (!result) {
-        die("Query tp show fields from table failed");
-    }
+// sending query
+ $result = mysql_query("SELECT * FROM {$table}");
+ if (!$result) {
+     die("Query to show fields from table failed");
+ }
 
-    $fields_num = mysql_num_fields($result);
+$fields_num = mysql_num_fields($result);
 
-    echo "<h1>Table: {$table}</h1>";
-    echo "<table border='1'><tr>";
-    //printing table headers
-    for ($i=0; $i<$fields_num; $i++){
-        $field = mysql_fetch_field($result);
-        echo "<td>{$field->name}</td>";
-    }
+
+echo "<h1>Table: {$table}</h1>";
+ echo "<table border='1'><tr>";
+ // printing table headers
+ for($i=0; $i<$fields_num; $i++)
+ {
+     $field = mysql_fetch_field($result);
+     echo "<td>{$field->name}</td>";
+ }
+ echo "</tr>\n";
+ // printing table rows
+ while($row = mysql_fetch_row($result))
+ {
+     echo "<tr>";
+
+    // $row is array... foreach( .. ) puts every element
+     // of $row to $cell variable
+     foreach($row as $cell)
+         echo "<td>$cell</td>";
+
     echo "</tr>\n";
-    //printing tbale rows
-    while($row = mysql_fetch_row($result)) {
-        echo "<tr>";
-        // $row is array... foreach(...) puts evr element
-        // of $row to $cell variable
-        foreach($row as $cell)
-            echo "<td>$cell</td>";
-        echo "</tr>\n";
-    }
-    mysql_free_result($result);
+ }
+ mysql_free_result($result);
+?>
+
+ <form>
+    Email: <input type="email" name="Email"><br>
+
+<?php
+        $email=$_POST['Email'];
+        mysql_connect("sql303.unaux.com", "unaux_23766826", "73oj1ns9");
+        mysql_select_db("unaux_23766826_mydatabase");
+        mysql_query("DELETE FROM testing WHERE Eamil='$email'")
+        or die(mysql_error());
+        echo "Completed!";
     ?>
-    </body>
-    </html>
+
+</body>
+</html>
